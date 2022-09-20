eeb70cc8fa351a1c7573a871825ea087f3959a3c
MeGallin <me@garyallin.uk>
2022-09-20 11:26:28 +0100

    working

---
+++ src/components/Header/HeaderComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './HeaderComponent.scss';
+
+export const HeaderComponent = () => {
+  return (
+    <div className="header__wrapper">
+      <h1>Multiplication</h1>
+    </div>
+  );
+};
